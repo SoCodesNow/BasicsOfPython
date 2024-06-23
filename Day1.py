fd8281bcad96ee2154bb3b2b1c4ecb551743e4dd
SoCodesNow <socodestales@gmail.com>
2024-06-23 15:56:16 +0530

    This is first commit

---
+++ Day1.py
@@ -0,0 +1,19 @@
+# Variables
+Name = "SoCodes"
+Age = "01"
+
+print("Name is - ", Name)
+print("Age is - ", Age)
+
+
+print("--------------------------")
+print("-------typecasting--------")
+# TypeCasting
+
+Weight_Char = str(60)
+Age = float(1.365782)
+Temp = int(33.5)
+
+print(Weight_Char)
+print(Age)
+print(Temp)
