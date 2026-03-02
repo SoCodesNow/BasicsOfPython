8b748d803f983b59f192f9db701231039686f45c
SoCodesNow <socodestales@gmail.com>
2026-03-02 19:49:09 +0530

    This is the second part of Day1 Learning about the python standard library

---
+++ First.py
@@ -0,0 +1,9 @@
+# assigning multiple values
+# unpacking
+# If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking.
+
+fruits = ["apple", "guava", "grapes"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
